71e2c0c47b8867bde2d878e940f37c0c5b771720
Vasu <mcac0931@gmail.com>
2024-08-02 16:59:18 +0530

    scecond commit

--- src/components/Resetpassword.jsx
+++ src/components/Resetpassword.jsx
@@ -22,7 +22,7 @@ export default function Resetpassword() {
     }
     else if (!validatePassword(password)) {
       // toast.error("Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character");
-      toast.error("Please Enter Valid Password");
+      toast.error("Please Entering Valid Password");
     }
 
   }
